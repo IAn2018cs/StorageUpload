eeeff0ae7d3c56b124e517024cc4048fc5c7eea0
chenshuai <chenshuai@amberweather.com>
2023-12-05 10:12:29 +0800

    修改上传文件夹

--- main.py
+++ main.py
@@ -28,5 +28,12 @@ if __name__ == '__main__':
     for file_name in os.listdir('files'):
         if '.DS' in file_name:
             continue
-        upload_path = f"files/{file_name}"
-        upload_storage(upload_path, 'foocus_models')
+        upload_path = f'files/{file_name}'
+        if os.path.isdir(upload_path):
+            for inner_file in os.listdir(upload_path):
+                if '.DS' in inner_file:
+                    continue
+                inner_path = f'{upload_path}/{inner_file}'
+                upload_storage(inner_path, f'foocus_models/{file_name}/{inner_file}')
+        else:
+            upload_storage(upload_path, f'foocus_models/{file_name}')
